4f30fe619f53af4db3c3c8a502d2a944f95b3abb
Tanvi Sankhyan <38919631+tanvisankhyan@users.noreply.github.com>
2023-04-18 12:19:51 -0400

    Add files via upload

---
+++ NYTarticles.py
@@ -0,0 +1,25 @@
+import os
+import openai
+
+
+openai.api_key="----------------------------"
+
+def article(query):
+  result = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Write an New York Times article: {}".format(query),
+  temperature=0.7,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+  )
+  print(result)
+  if 'choices' in result:
+    if (len(result['choices']) > 0):
+      answer = result['choices'][0]['text']
+      print(answer)
+
+
+
+article("Cognitive Dissonance")
